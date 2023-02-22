859812e2a615fcc8f6da55c991d6a08768880a25
Tyler Coats <coatsctyler@gmail.com>
2023-02-21 22:43:15 -0500

    Fixed Nav to links

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -10,9 +10,9 @@ const Nav = () => {
         <nav className='nav'>
             <ul className='nav-menu'>
                 <li className='nav-item'><Link className='nav-links' to='/'><button className='link-btn'>Home</button></Link></li>
-                <li className='nav-item'><Link className='nav-links' to='/about'><button className='link-btn'>Parks List</button></Link></li>
-                <li className='nav-item'><Link className='nav-links' to='/parks-list'><button className='link-btn'>Rides List</button></Link></li>
-                <li className='nav-item'><Link className='nav-links' to='/rides-list'><button className='link-btn'>About</button></Link></li>
+                <li className='nav-item'><Link className='nav-links' to='/parks-list'><button className='link-btn'>Parks List</button></Link></li>
+                <li className='nav-item'><Link className='nav-links' to='/rides-list'><button className='link-btn'>Rides List</button></Link></li>
+                <li className='nav-item'><Link className='nav-links' to='/about'><button className='link-btn'>About</button></Link></li>
             </ul>
         </nav>
     </div>
