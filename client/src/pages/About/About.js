3b454ac129785559544587214508643acb42bae7
Bondarni <118389811+Bondarni@users.noreply.github.com>
2023-02-22 08:58:43 -0500

    added text to About section

---
+++ client/src/pages/About/About.js
@@ -0,0 +1,18 @@
+const About = () => {
+  return (
+    <div className="about-section">
+      <h1>Explore an Abandoned Park Today!</h1>
+      <p>
+        These parks may be closed, but they still have a story to tell. Use this
+        website to catalogue and share some of the country's greatest monuments
+        to economic ruin.
+      </p>
+      <p>
+        Grab a friend, a flashlight, and maybe a tetanus shot; the admission is
+        free, after all.
+      </p>
+    </div>
+  )
+}
+
+export default About
