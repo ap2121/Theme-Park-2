1489b2de4b1916b949aeaeab226efb2625d90f06
Bondarni <118389811+Bondarni@users.noreply.github.com>
2023-02-21 19:40:13 -0500

    added boilerplate ParkCard component and linked to ParkList page

---
+++ client/src/components/ParkCard/ParkCard.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+const ParkCard = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  )
+}
+
+export default ParkCard
