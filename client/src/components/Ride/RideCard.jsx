6b16e504ac4eed2f8dfb6aedbc1588a06823d456
Arthur Pate <arthurpate42@gmail.com>
2023-02-21 19:27:39 -0800

    updated size of photos

--- client/src/components/Ride/RideCard.jsx
+++ client/src/components/Ride/RideCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import './RideCard.css'
 const RideCard = (props) => {
   return (
     <div>
         <h3>{props.name}</h3>
-        <img src={props.image} alt={props.name}/>
         <p>{props.description}</p>
+        <img src={props.image} alt={props.name}/>
+        
+        
     </div>
   )
 }
