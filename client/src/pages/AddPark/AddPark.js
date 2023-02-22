e19f3ae34933f13f9eed08eed52d6c19b39ebab0
Arthur Pate <arthurpate42@gmail.com>
2023-02-22 15:47:02 -0800

    redoing the button

---
+++ client/src/pages/AddPark/AddPark.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+const AddPark = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: ""
+  })
+  
+    return (
+    <div>AddPark</div>
+  )
+}
+
+export default AddPark
