f09feb7855060f1556616b10617d5df529c9f339
Arthur Pate <arthurpate42@gmail.com>
2023-02-22 16:02:09 -0800

    updated nav

--- client/src/pages/AddPark/AddPark.js
+++ client/src/pages/AddPark/AddPark.js
@@ -5,9 +5,27 @@ import {useNavigate} from 'react-router-dom'
 const AddPark = () => {
   const [formData, setFormData] = useState({
     name: "",
-    image: ""
+    image: "",
+    location: "",
+    description: "",
+    dateClosed: ""
   })
   
+const navigate = useNavigate()
+const handleChange = (e) => {
+    setFormData((prevFormData) => {
+        return {
+            ...prevFormData,
+            [e.target.name]: e.target.value
+        }
+    })
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post('api/parks', {...formData})
+    navigate('parks-list')
+} 
     return (
     <div>AddPark</div>
   )
