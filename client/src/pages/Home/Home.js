ff83b68ea9900ed1791caa7cd49ec3edc8f243ef
Tyler Coats <coatsctyler@gmail.com>
2023-02-23 10:12:10 -0500

    Added title

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,7 +3,12 @@ import './Home.css'
 const Home = () => {
   return (
     <div className="home-container">
-        
+        <div className="title-cnt">
+            <div className="title">
+                <h1>These parks may be closed...</h1>
+                <h2>...But the gates are open.</h2>
+            </div>
+        </div>
     </div>
   )
 }
